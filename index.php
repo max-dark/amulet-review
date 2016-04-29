e32beae254c0c8c7324346ba24501dc4c44da694
Max Dark <maxim.dark@gmail.com>
2016-04-29 10:51:44 +0300

    remove unused code

--- index.php
+++ index.php
@@ -6,21 +6,14 @@ header ("Cache-Control: no-cache, no-store, must-revalidate, max-age=0");
 header ("Pragma: no-cache");
 header("Content-type:text/vnd.wap.wml;charset=utf-8");
 
-echo "<?xml version=\"1.0\" ?>\n";?>
+?><?xml version="1.0" ?>
 <!DOCTYPE wml PUBLIC "-//WAPFORUM//DTD WML 1.1//EN" "http://www.wapforum.org/DTD/wml_1.1.xml"> 
-<?php setlocale (LC_CTYPE, 'ru_RU.CP1251'); 
-function win2unicode ( $s ) { if ( (ord($s)>=192) & (ord($s)<=255) ) $hexvalue=dechex(ord($s)+848); if ($s=="Ё") $hexvalue="401"; if ($s=="ё") $hexvalue="451"; return("&#x0".$hexvalue.";");} 
-function translate($s) {return(preg_replace("/[А-яЁё]/e","win2unicode('\\0')",$s));} 
-ob_start("translate");
-?>
 <wml>
-<card title="Амулет Дракона" ontimer="1/g.php">
+<card title="РђРјСѓР»РµС‚ Р”СЂР°РєРѕРЅР°" ontimer="1/g.php">
 <timer value="1"/>
 <p>
-Если вас автоматически не перенаправляет на другую страницу, нажмите эту 
-<a href="1/g.php">ссылку</a>.
+Р•СЃР»Рё РІР°СЃ Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРё РЅРµ РїРµСЂРµРЅР°РїСЂР°РІР»СЏРµС‚ РЅР° РґСЂСѓРіСѓСЋ СЃС‚СЂР°РЅРёС†Сѓ, РЅР°Р¶РјРёС‚Рµ СЌС‚Сѓ 
+<a href="1/g.php">СЃСЃС‹Р»РєСѓ</a>.
 </p>
 </card>
 </wml>
-<?
-ob_end_flush();die("");
