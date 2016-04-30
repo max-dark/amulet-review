7357708fc936ee03df5829f7584d57e9e021ded9
Max Dark <maxim.dark@gmail.com>
2016-04-30 20:33:07 +0300

    index.php wml -> html

--- index.php
+++ index.php
@@ -4,16 +4,17 @@ header("Expires: Thu, 01 Jan 1970 00:00:01 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-cache, no-store, must-revalidate, max-age=0");
 header("Pragma: no-cache");
-header("Content-type:text/vnd.wap.wml;charset=utf-8");
-
-echo '<?xml version="1.0"?>'?>
-<!DOCTYPE wml PUBLIC "-//WAPFORUM//DTD WML 1.1//EN" "http://www.wapforum.org/DTD/wml_1.1.xml">
-<wml>
-    <card title="Амулет Дракона" ontimer="1/g.php">
-        <timer value="1"/>
-        <p>
-            Если вас автоматически не перенаправляет на другую страницу, нажмите эту
-            <a href="1/g.php">ссылку</a>.
-        </p>
-    </card>
-</wml>
+header("Content-type:text/html;charset=utf-8");
+?><!DOCTYPE html>
+<html>
+<head>
+    <title>Амулет Дракона</title>
+    <meta http-equiv="refresh" content="5;/game/g.php" />
+</head>
+<body>
+    <p>
+        Если вас автоматически не перенаправляет на другую страницу, нажмите эту
+        <a href="/game/g.php">ссылку</a>.
+    </p>
+</body>
+</html>
