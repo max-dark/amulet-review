2e13229fc67619c4cbd1cdfef3189f0fd9f90e29
Max Dark <maxim.dark@gmail.com>
2016-05-01 02:54:16 +0300

    make IDE happy - define type of global varibles

---
+++ game/modules/globals.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Make IDE happy )
+ * define global variables
+ */
+
+/* @var string $sid Индификатор пользователя */
+$sid = '';
+/* @var string $site Страница для  перехода */
+$site = '';
+/* @var string $gm какой то индификатор */
+$gm = '';
+/* @var array $game Состояние игры */
+$game = [];
