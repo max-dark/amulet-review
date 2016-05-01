d94ea7762d2cdff788a6e26e8c306ce63bd94313
Max Dark <maxim.dark@gmail.com>
2016-05-01 10:54:12 +0300

    add dir_foreach function

--- game/modules/file_functions.php
+++ game/modules/file_functions.php
@@ -1,7 +1,18 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: max
- * Date: 01.05.16
- * Time: 10:20
+ * Вызывает $callback для каждого элемента(кроме списка $exclude) в $dirname
+ *
+ * @param $dirname string directory for scan
+ * @param $callback callable callback
+ * @param $exclude array list to exclude
  */
+function dir_foreach($dirname, $callback, $exclude = []) {
+    $dh = opendir($dirname);
+    /* @var string $f_name */
+    while (($f_name = readdir($dh)) !== false) {
+        if(!in_array($f_name, $exclude)) {
+            $callback($f_name);
+        }
+    }
+    closedir($dh);
+}
