87584e4bb40a87490a8084dc874617242d01b5d6
Max Dark <maxim.dark@gmail.com>
2016-06-11 08:27:56 +0300

    add DB singleton

---
+++ game/modules/database.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Class DB
+ */
+class DB {
+    /** @var \PDO $link*/
+    private static $link = null;
+
+    /**
+     * @param array|null $config database config
+     * @return \PDO link to database
+     * @throws \PDOException if connect fail
+     */
+    public static function link($config = null) {
+        if (is_null(self::$link)) {
+            if (is_array($config)) {
+                self::$link = new \PDO(
+                    sprintf('mysql:%s;dbname=%s;charset=utf8',
+                        $config['server'],
+                        $config['dbname']
+                    ),
+                    $config['login'],
+                    $config['password'],
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                    ]
+                );
+            }
+        }
+        return self::$link;
+    }
+}
