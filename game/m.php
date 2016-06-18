52a7dfb8cf3bffbf10d67ad361f5ee8826e10eb1
Max Dark <maxim.dark@gmail.com>
2016-06-18 19:51:39 +0300

    refactor show_map function

--- game/m.php
+++ game/m.php
@@ -3,6 +3,9 @@
  * карта, передается map=x123x456 и для картики img=1 bw=1|2|3, f=x123x456 место флага
  */
 
+require_once 'modules/image.php';
+
+
 if (array_key_exists('info', $_GET)) {
     msg_m("Ваша позиция обозначена белым квадратиком с черной точкой в центре (или черным квадратиком с белой точкой в центре, в зависимости от фона).<br/>Флаг лидерства - для цветных желтым квадратиком с черной точкой, для ч/б - как ваша.<br/>Замки - красные квадратики с белой точкой в центре (только для цветной карты).<br/>В настройках можете поменять тип карты (формат PNG лучший, но не все телефоны поддерживают).<br/><a href=\"http://mag.su/game/f_faq.php?id=map\">Другие карты</a>");
 }
@@ -18,7 +21,7 @@ if (array_key_exists('img', $_GET)) {
  * @param string $location
  * @return array
  */
-function calctc($location)
+function calculateCoordinates($location)
 {
 
     switch (substr($location, 0, 4)) {
@@ -39,22 +42,23 @@ function calctc($location)
             if ($location == "arena")  $location = "x1086x501";
     }
 
-    $tc = explode("x", $location);
-    if ($tc[2] > 1101) {
-        $tc[1] = round(($tc[1] - 20) / 6);
-        $tc[2] = round(($tc[2] - 1101) / 6);
-        $tc[3] = 2;
+    /** @noinspection PhpUnusedLocalVariableInspection */
+    list($type, $x, $y) = explode("x", $location);
+    if ($y > 1101) {
+        $x = round(($x - 20) / 6);
+        $y = round(($y - 1101) / 6);
+        $type = 2;
     } else
-        if ($tc[1] > 1650) {
-            $tc[1] = round(($tc[1] - 450 - 1200) / 15);
-            $tc[2] = round($tc[2] / 15);
-            $tc[3] = 1;
+        if ($x > 1650) {
+            $x = round(($x - 450 - 1200) / 15);
+            $y = round($y / 15);
+            $type = 1;
         } else {
-            $tc[1] = round(($tc[1] - 450) / 12);
-            $tc[2] = round($tc[2] / 12);
-            $tc[3] = 0;
+            $x = round(($x - 450) / 12);
+            $y = round($y / 12);
+            $type = 0;
         }
-    return $tc;
+    return array($type, $x, $y);
 }
 
 
@@ -63,73 +67,84 @@ function calctc($location)
  * @param string $flag локация флага
  * @param int $image_type формат изображения
  */
-function show_map($char, $flag, $image_type)
-{
-    $char_coord = calctc($char);
-    $flag_coord = calctc($flag);
-    switch ($image_type) {
-        case 1:
-            $file_type = "wbmp";
-            break;
-        case 2:
-            $file_type = "jpg";
-            break;
-        default:
-            $file_type = "png";
-            break;
-    }
-    $img_file = "map" . ($char_coord[3] + 1) . ".$file_type";
-    $size = getimagesize($img_file);
-    switch ($image_type) {
-        case 1:
-            $image = imagecreatefromwbmp($img_file);
-            break;
-        case 2:
-            $image = imagecreatefromjpeg($img_file);
-            break;
-        default:
-            $image = imagecreatefrompng($img_file);
-            break;
-    }
-    if (!$image) die("err");
-    $col = imagecolorat($image, $char_coord[1] + 1, $char_coord[2] + 1);
-    if ($col == 0 || $col == "16777180") {
-        $cb = 1;
-        $cc = imagecolorallocate($image, 255, 255, 255);
+function show_map($char, $flag, $image_type) {
+    list($char_map, $char_x, $char_y) = calculateCoordinates($char);
+    list($flag_map, $flag_x, $flag_y) = calculateCoordinates($flag);
+
+    $image = create_image($image_type, $char_map + 1);
+
+    $white_color = $image->allocateColor(0xff, 0xff, 0xff);
+    $black_color = $image->allocateColor(0, 0, 0);
+    $color = $image->colorAt($char_x + 1, $char_y + 1);
+    if ($color == 0 || $color == 0xFFFFDC) {
+        $bg_color = 1;
+        $fg_color = $white_color;
     } else {
-        $cb = imagecolorallocate($image, 255, 255, 255);
-        $cc = 1;
+        $bg_color = $white_color;
+        $fg_color = 1;
     }
-    imagefilledrectangle($image, $char_coord[1], $char_coord[2], $char_coord[1] + 2, $char_coord[2] + 2, $cb);
-    imagesetpixel($image, $char_coord[1] + 1, $char_coord[2] + 1, $cc);
-    if ($flag_coord[3] == $char_coord[3] && $flag != $char) {    // флаг
-        if ($col == 0 || $col == "16777180") {
-            $cb = 1;
-            $cc = imagecolorallocate($image, 255, 255, 255);
+    set_mark($image, $char_x, $char_y, $fg_color, $bg_color);
+    if ($flag_map == $char_map && $flag != $char) {    // флаг
+        if ($color == 0 || $color == 0xFFFFDC) {
+            $bg_color = 1;
+            $fg_color = $white_color;
         } else {
-            $cb = imagecolorallocate($image, 255, 255, 255);
-            $cc = 1;
+            $bg_color = $white_color;
+            $fg_color = 1;
         }
         if ($image_type != 1) {
-            $cb = imagecolorallocate($image, 255, 255, 0);
-            $cc = imagecolorallocate($image, 0, 0, 0);
+            $bg_color = $white_color;
+            $fg_color = $black_color;
         }
-        imagefilledrectangle($image, $flag_coord[1], $flag_coord[2], $flag_coord[1] + 2, $flag_coord[2] + 2, $cb);
-        imagesetpixel($image, $flag_coord[1] + 1, $flag_coord[2] + 1, $cc);
+        set_mark($image, $flag_x, $flag_y, $fg_color, $bg_color);
     }
-    header("Content-type: {$size['mime']}");    //;charset=utf-8
+    header("Content-type: {$image->getInfo('mime')}");
+    $image->writeFile();
+    unset($image);
+}
+
+/**
+ * @param Image $image
+ * @param int $x
+ * @param int $y
+ * @param int $fg_color
+ * @param int $bg_color
+ */
+function set_mark(&$image, $x, $y, $fg_color, $bg_color)
+{
+    $image->filledRectangle($x, $y, $x + 2, $y + 2, $bg_color);
+    $image->setPixel($x + 1, $y + 1, $fg_color);
+}
+
+/**
+ * @param int $image_type
+ * @param int $id
+ * @return Image
+ */
+function create_image($image_type, $id)
+{
     switch ($image_type) {
         case 1:
-            imagewbmp($image);
+            $image = new WBMPImage(build_name($id, "wbmp"));
             break;
         case 2:
-            imagejpeg($image);
+            $image = new JpegImage(build_name($id, "jpg"));
             break;
         default:
-            imagepng($image);
+            $image = new PNGImage(build_name($id, "png"));
             break;
     }
-    imagedestroy($image);
+    return $image;
+}
+
+/**
+ * @param int $id
+ * @param string $file_ext
+ * @return string
+ */
+function build_name($id, $file_ext)
+{
+    return "map{$id}.{$file_ext}";
 }
 
 /**
