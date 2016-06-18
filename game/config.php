f3a27d3a91cae83559c898faabc61eb999adda84
Max Dark <maxim.dark@gmail.com>
2016-06-18 15:24:29 +0300

    move header calls to msg function

--- game/config.php
+++ game/config.php
@@ -1,11 +1,7 @@
 <?php
-header("Expires: Thu, 01 Jan 2010 00:00:01 GMT");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-cache, no-store, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Content-type:text/vnd.wap.wml;charset=utf-8");
-//header( "Content-type:text/vnd.wap.wml" );
-echo "<?xml version='1.0'?><!DOCTYPE wml PUBLIC '-//WAPFORUM//DTD WML 1.3//EN' 'http://www.wapforum.org/DTD/wml13.dtd'>";
+/**
+ * @file game/config.php
+ */
 
 /** @var int $starttime */
 $starttime = microtime(1);
