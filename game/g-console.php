d30c8c426f09469b970a75eccc196fe2163a3304
Max Dark <maxim.dark@gmail.com>
2016-06-24 00:27:02 +0300

    fix console tool

--- game/g-console.php
+++ game/g-console.php
@@ -3,6 +3,7 @@ $QUERY_STRING = '';
 $args = array_slice($argv, 1);
 if(count($args)) {
     $QUERY_STRING = implode('&', $args);
+    parse_str($QUERY_STRING, $_REQUEST);
 }
 $_SERVER["QUERY_STRING"] = $QUERY_STRING;
 require_once 'g.php';
