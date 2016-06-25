6c3a93a10edfa3fbee6217a2526efe97fcbccc89
Max Dark <maxim.dark@gmail.com>
2016-06-25 14:29:06 +0300

    fix links

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ header("Content-type:text/html;charset=utf-8");
 <html>
 <head>
     <title>Амулет Дракона</title>
-    <meta http-equiv="refresh" content="5;/game/g.php" />
+    <meta http-equiv="refresh" content="5;/game/" />
 </head>
 <body>
     <p>
         Если вас автоматически не перенаправляет на другую страницу, нажмите эту
-        <a href="/game/g.php">ссылку</a>.
+        <a href="/game/">ссылку</a>.
     </p>
 </body>
 </html>
